4c07f594adce47bc1fd4a2f64f4d1564fd9df1c8
Mateus Mourão <99206764+matmourao@users.noreply.github.com>
2023-06-16 18:58:10 -0300

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Node
+{
+    int iData;
+    Node* ptrLeft;
+    Node* ptrRight;
+};
+
+Node* newNode(int iVal)
+{
+    Node* ptrTemp = (Node*) malloc(sizeof(Node));
+    ptrTemp -> ptrRight = nullptr;
+    ptrTemp -> ptrLeft = nullptr;
+    ptrTemp -> iData = iVal;
+    return ptrTemp;
+}
+
+Node* insertTree(Node* no, int iVal)
+{
+    if(no == nullptr) return newNode(iVal);
+    
+    if(iVal < no->iData)
+    {
+        no->ptrLeft = insertTree(no->ptrLeft, iVal);
+    }    
+    else
+    {
+        no->ptrRight = insertTree(no->ptrRight, iVal);
+    }
+
+    return no;
+}
+
+Node* buildFile()
+{
+    Node* root = nullptr;
+
+    string strPath;
+    cout << "Digite o path do arquivo: ";
+    cin >> strPath;
+
+    fstream fsFile(strPath);
+    string strVal;
+    while(getline(fsFile, strVal))
+    {
+        root = insertTree(root, stoi(strVal));
+    }
+    
+    cout << "Árvore criada!" << endl;
+    return root;
+}
+
+Node* buildInput()
+{
+    cout << "Digite um a um os elementos da árvore" << endl;
+    cout << "Digite 'N' quando tiver acabado" << endl;
+
+    Node* root = nullptr;
+
+    while(true)
+    {
+        string strInput;
+        cin >> strInput;
+
+        if(strInput == "N") break;
+
+        root = insertTree(root, stoi(strInput));
+    }
+
+    cout << "Árvore criada!" << endl;
+    return root;
+}
+
+Node* buildTree()
+{
+    cout << "Para construir uma árvore através de arquivo, digite 'file' " << endl;
+    cout << "Para construir uma árvore através de input, digite 'input' " << endl;
+
+    string strInput;
+    cin >> strInput;
+
+    if(strInput == "file")
+    {
+        return buildFile();
+    }
+
+    if(strInput == "input")
+    {
+        return buildInput();
+    }
+
+    cout << "Input inválido!" << endl;
+    return buildTree();
+}
+
+void printInstruc()
+{
+    cout << "Instruções" << endl;
+}
+
+void menu(Node* root)
+{
+    printInstruc();
+
+    while(true)
+    {
+        string strInput;
+        cin >> strInput;
+
+        if(strInput == "exit")
+        {
+            return;
+        }
+
+        if(strInput == "instructions")
+        {
+            printInstruc();
+            continue;
+        }
+
+        if(strInput == "altura")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "tamanho")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "insert")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "remove")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "search")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "completa")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "perfeita")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "print")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "bubble")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "selection")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "insertion")
+        {
+            //
+            continue;
+        }
+
+        if(strInput == "shell")
+        {
+            //
+            continue;
+        }
+
+        cout << "Input inválido! Digite 'instructions' para rever as instruções." << endl;
+    }
+}
+
+int main()
+{
+    cout << "Bem vindo!" << endl;
+
+    Node* root = buildTree();
+
+    menu(root);
+
+    cout << "Tchau!" << endl;
+
+    return 0;
+}
