6d8bcdf4b76b7779a3fe8cf8303c0622de738437
Luana Hatab <99187348+luanahatab@users.noreply.github.com>
2023-06-30 14:59:49 -0300

    add comments

--- sources/list.cpp
+++ sources/list.cpp
@@ -86,6 +86,7 @@ int lenght(List** head)
 
 void bubbleSort(struct List** head)
 {
+    // percorre a lista para descobrir o tamanho
     struct List* ptrTemp = *head;
     int iSize = 0;
 
@@ -94,14 +95,15 @@ void bubbleSort(struct List** head)
         ptrTemp = ptrTemp -> ptrNext;
         iSize++;
     }
-
+    
+    // cria loop a partir do tamanho
     for(int i = 1; i < iSize; i++)
     {
         ptrTemp = *head;
         bool Swapped = false;
         for(int j = 0; j < iSize - i; j++)
         {
-            if(ptrTemp -> iData > ptrTemp -> ptrNext -> iData)
+            if(ptrTemp -> iData > ptrTemp -> ptrNext -> iData) // se itens consecutivos não estiverem ordenados, os nós serão trocados
             {
                 displayList(*head);
                 swapNext(head, ptrTemp);
@@ -116,23 +118,25 @@ void bubbleSort(struct List** head)
 
 void selectionSort(struct List** head)
 {
-    struct List* ptrTemp1 = (*head);
+    struct List* ptrTemp1 = (*head); // ponteiro temporário para loop externo
 
     while(ptrTemp1 != nullptr)
     {
-        struct List* ptrTemp2 = ptrTemp1 -> ptrNext;
-        struct List* ptrMin = ptrTemp1;
+        struct List* ptrTemp2 = ptrTemp1 -> ptrNext; // ponteiro temporário para loop interno
+        struct List* ptrMin = ptrTemp1;  // ponteiro criado para armazenar o valor mínimo
 
         while(ptrTemp2 != nullptr)
         {
             if(ptrMin -> iData > ptrTemp2 -> iData)
             {
-                ptrMin = ptrTemp2;
+                ptrMin = ptrTemp2; // se ptrTemp2 é menor que o antigo menor valor, ele passa a ser no novo mínimo
             }
             ptrTemp2 = ptrTemp2 -> ptrNext;
         }
 
         displayList(*head);
+        // após todas as iterações (percorrer a lista de ptrTemp1 até nullptr), ptrMin será o menor elemento da lista a partir de ptrTemp1
+        // logo, iremos trocar eles de posição para ordenar a lista de maneira crescente
         swapNodes(head, ptrMin, ptrTemp1);
 
         ptrTemp1 = ptrMin -> ptrNext;
