0a25d3baf0f702f8d09c602d7442858f7645d994
Guilherme Queiroz Corrêa Lima <guilhermeq700@gmail.com>
2023-06-30 01:26:09 -0300

    Update list.cpp
    Mudança temporaria, apenas para poder testar a representação grafica com mais facilidade

--- sources/list.cpp
+++ sources/list.cpp
@@ -2,6 +2,119 @@
 #include <cmath>
 #include "../headers/list.hpp"
 
+
+
+
+
+// ========================================================================
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+// Função para calcular o número de dígitos de um número
+int numDigits(int number)
+{
+    int digits = 0;
+
+    while (number)
+    {
+        number /= 10;
+        digits++;
+    }
+
+    return digits;
+}
+
+// Função para calcular o número de hashtags para cada valor na lista vinculada
+map<int, int> calculateHashes(struct List* head)
+{
+    // Cria um vetor para armazenar os valores dos nós
+    vector<int> values;
+    struct List* node = head;
+
+    // Percorre a lista vinculada e adicionar os valores ao vetor
+    while (node != nullptr)
+    {
+        values.push_back(node->iData);
+        node = node->ptrNext;
+    }
+
+    // Ordena o vetor em ordem crescente
+    sort(values.begin(), values.end());
+
+    // Cria um mapa para armazenar o valor e o número de hashtags correspondente
+    map<int, int> hashes;
+
+    // Atribui o número de hashtags para cada valor
+    for (vector<int>::size_type i = 0; i < values.size(); ++i)
+    {
+        hashes[values[i]] = i + 1;
+    }
+
+    return hashes;
+}
+
+void displayList(struct List* head)
+{
+    // Calcule o número de hashtags para cada valor
+    map<int, int> hashes = calculateHashes(head);
+
+    // Encontra o valor máximo no mapa de hashes
+    // Fazemos isso para que nunca tenhamos barras verticais desalinhadas com os números
+    int max_val = 0;
+    int max_digits = 0;
+    for (auto& pair : hashes)
+    {
+        if (pair.second > max_val)
+            max_val = pair.second;
+        int digits = numDigits(pair.first);
+        if (digits > max_digits)
+            max_digits = digits;
+    }
+
+    // Imprime as barras
+    for (int height = max_val; height >= 1; --height)
+    {
+        struct List* node = head;
+        while (node != nullptr)
+        {
+            if (hashes[node->iData] >= height)
+                cout << string(max_digits, 'O') + " ";
+            else
+                cout << string(max_digits, ' ') + " ";
+            node = node->ptrNext;
+        }
+        cout << endl;
+    }
+
+    // Imprime os números
+    struct List* node = head;
+    while (node != nullptr)
+    {
+        cout << setw(max_digits) << node->iData << ' ';
+        node = node->ptrNext;
+    }
+    cout << endl;
+
+    // Espera por meio segundo e apaga 
+    this_thread::sleep_for(chrono::milliseconds(500));
+
+   system("clear||cls");
+}
+
+// ========================================================================
+
+
+
+
+
 List* newList(int iVal)
 {
     List* ptrNewList = new List;
@@ -99,6 +212,7 @@ void bubbleSort(struct List** head)
         {
             if(ptrTemp->iData > ptrTemp->ptrNext->iData)
             {
+                displayList(*head);
                 swapNext(head, ptrTemp);
                 Swapped = true;
             }
@@ -126,6 +240,7 @@ void selectionSort(struct List** head)
             ptrTemp2 = ptrTemp2->ptrNext;
         }
 
+        displayList(*head);
         swapNodes(head, ptrMin, ptrTemp1);
 
         ptrTemp1 = ptrMin->ptrNext;
@@ -140,6 +255,7 @@ void insertSort(List** head)
         ptrTemp = ptrTemp->ptrNext;
         while(ptrTemp->iData < ptrTemp->ptrPrev->iData)
         {
+            displayList(*head);
             swapNodes(head, ptrTemp, ptrTemp->ptrPrev);
             if(ptrTemp->ptrPrev == nullptr) break;
         }
@@ -181,6 +297,7 @@ void shellSort(List** head)
                 
                 while(ptrTemp2->iData < ptrTemp1->iData)
                 {
+                    displayList(*head);
                     swapNodes(head, ptrTemp1, ptrTemp2);
                     //garante que os ponteiros continuem apontando para o mesmo indice da lista
                     if(ptrTemp1 == ptrFirst) ptrFirst = ptrTemp2;
