0ca276cba550ee282cebf9a0844f342dd533e393
Luiza Schneider Sampaio <98579033+luizasch2@users.noreply.github.com>
2023-06-30 14:19:50 -0300

    adicionando comentários

--- sources/queue.cpp
+++ sources/queue.cpp
@@ -7,41 +7,45 @@ QueueNode* rear = nullptr;
 // Implementação da função enfileirar
 void enQueue(Node* treeNode) 
 {
-    QueueNode* temp = new QueueNode();
-    temp->treeNode = treeNode;
-    temp->next = nullptr;
+    QueueNode* temp = new QueueNode(); // Cria uma nova queueNode
+    temp->treeNode = treeNode; // Atribui o valor do nó da árvore à queueNode
+    temp->next = nullptr; // Atribui nullptr ao ponteiro next da queueNode
 
-    if(front == nullptr && rear == nullptr) {
-        front = rear = temp;
+    if(front == nullptr && rear == nullptr) 
+    {
+        front = rear = temp; // Se a fila está vazia, front e rear apontam para a nova queueNode
         return;
     }
 
-    rear->next = temp;
-    rear = temp;
+    rear->next = temp; // O ponteiro next do último elemento da fila aponta para a nova queueNode
+    rear = temp; // Rear aponta para a nova queueNode
 }
 
 // Implementação da função desenfileirar
 Node* deQueue() 
 {
-    if(front == nullptr) {
+    // Se a fila está vazia, retorna nullptr
+    if(front == nullptr) 
+    {
         return nullptr;
     }
 
-    QueueNode* temp = front;
-    Node* nodeToReturn = temp->treeNode;
+    QueueNode* temp = front; // Cria um ponteiro para o primeiro elemento da fila
+    Node* nodeToReturn = temp->treeNode; // Cria um ponteiro para o nó da árvore que será retornado
 
-    front = front->next;
-    if(front == nullptr) {
-        rear = nullptr;
+    front = front->next; // Front aponta para o segundo elemento da fila
+    if(front == nullptr) 
+    {
+        rear = nullptr; // Se a fila está vazia, rear aponta para nullptr
     }
 
-    delete temp;
+    delete temp; // Deleta o primeiro elemento da fila
 
-    return nodeToReturn;
+    return nodeToReturn; // Retorna o nó da árvore
 }
 
 // Implementação da função para verificar se a fila está vazia
 bool isQueueEmpty() 
 {
-    return front == nullptr;
+    return front == nullptr; // Se front aponta para nullptr, a fila está vazia
 }
